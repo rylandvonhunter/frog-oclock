4d6c7a5af91389d7d0f970e1a958feb6e52f36be
Ryland Von Hunter <ryland.vonhunter@icloud.com>
2025-05-04 16:12:53 -0400

    frogoclock menu done
    -need to make it play the game
    +credits+options need to be done.
    
    END

--- assets/main.lua
+++ assets/main.lua
@@ -1,20 +1,39 @@
--- Frog O'clock
+-- Frog o'clock
 --
 function _init()
   world.info("init XXX")
 end
 
 function _update()
-end
-x = 0
-y = 0
+    if btnp(3) and selection <3 then
+        selection = selection + 1
+    end
+    if btnp(2) and selection > 0
+        then
 
+        selection = selection - 1
+    end
+end
+selection = 0
 function _draw()
-  cls(0)
-  print("frog o'clock", x, y, 7)
-  y = y + 1
-  if y > 128 then
-    y = 0
-    x = x + 1
-  end
+  cls()
+  color(0)
+  spr(0)
+  local cx, cy = 110, 68
+  --circfill(cx, cy, 17, 0)
+  local t = time() / 10
+  local l = 20
+  line(cx, cy, cx + l * sin(t), cy + l * cos(t), 0)
+  t = time() / 600
+  l = 10
+  line(cx, cy, cx + l * sin(t), cy + l * cos(t), 0)
+  print("play", 51, 41)
+
+print("options", 46, 53)
+
+print("quit", 51, 65)
+
+print("credits", 46, 77)
+
+circfill(40, 43 + 12 * selection, 1)
 end
